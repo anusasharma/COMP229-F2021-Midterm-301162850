a31bf67de9cb8bad1c958a45465adafb69c3176c
Anusa <anusoruban@gmail.com>
2021-11-01 18:21:34 +0530

    COMP229-M2021

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  // "URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://anusa:akHeHZK9OxRmvKTE@cluster0.i77ti.mongodb.net/book_store?retryWrites=true&w=majority",
+};
